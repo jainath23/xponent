84c69c79242dd6cb453c4b6a705c66021d5ac83c
Jai Nath Gupta <jainath2312@gmail.com>
2017-06-18 14:16:39 +0530

    Second commit of message

--- DemoGit/src/com/jai/MyDemo.java
+++ DemoGit/src/com/jai/MyDemo.java
@@ -5,7 +5,7 @@ public class MyDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi Git Hub");
-
+		System.out.print("Second commoit");
 	}
 
 }
